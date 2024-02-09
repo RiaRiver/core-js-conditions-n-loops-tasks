1ec45671368bfc55f2629f9c708df6ad3e1a9c1a
Ria River <riria.river@gmail.com>
2024-02-09 09:13:36 +0300

    feat: implement isContainNumber function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -282,8 +282,18 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let currentNum = num;
+
+  while (currentNum) {
+    if (currentNum % 10 === digit) {
+      return true;
+    }
+
+    currentNum = Math.floor(currentNum / 10);
+  }
+
+  return false;
 }
 
 /**
